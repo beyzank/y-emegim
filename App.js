ac6bf169b3526150a18bb175520e5f07b2e17739
Beyza Nur Kahraman <beyzanurkahraman.11@gmail.com>
2023-01-17 18:41:47 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,40 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  useColorScheme,
+  Text
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <Text>Y-EMEĞİM</Text>
+    </SafeAreaView>
+  );
+};
+
+
+
+export default App;
